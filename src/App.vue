fd610355b59b3105672c6e20297c67e059a3d005
Chianing <chianinghan@didiglobal.com>
2022-02-01 00:23:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <el-container>
+            <el-header>
+                <navigation-header/>
+            </el-header>
+
+            <el-container>
+                <el-aside width="200px">
+                    <navigation-left/>
+                </el-aside>
+
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+
+import NavigationHeader from "@/components/common/NavigationHeader";
+import NavigationLeft from "@/components/common/NavigationLeft";
+
+export default {
+    name: 'App',
+    components: {
+        NavigationLeft,
+        NavigationHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
